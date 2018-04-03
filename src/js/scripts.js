6191f89103bc8d01d92e7670bacd81f29a3bd318
Adrian Pawlak <bebeto9009@gmail.com>
2018-04-03 23:01:34 +0200

    fix menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,11 +8,43 @@
    });
 })()
 /* end menu */
+//
+// var grid = document.querySelector('.grid');
+// var msnry;
+//
+// imagesLoaded( grid, function() {
+//     // init Isotope after all images have loaded
+//     msnry = new Masonry( grid, {
+//         itemSelector: '.grid-item',
+//         columnWidth: '.grid-sizer',
+//         percentPosition: true
+//     });
+// });
 
-var $grid = $('.grid').imagesLoaded( function() {
-    $grid.masonry({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        columnWidth: '.grid-sizer'
+function masonry(grid, gridCell, gridGutter, dGridCol, tGridCol, mGridCol) {
+    var g = document.querySelector(grid),
+        gc = document.querySelectorAll(gridCell),
+        gcLength = gc.length,
+        gHeight = 0,
+        i;
+
+    for(i=0; i<gcLength; ++i) {
+        gHeight+=gc[i].offsetHeight+parseInt(gridGutter);
+    }
+
+    if(window.screen.width >= 1024)
+        g.style.height = gHeight/dGridCol + gHeight/(gcLength+1) + "px";
+    else if(window.screen.width < 1024 && window.screen.width >= 768)
+        g.style.height = gHeight/tGridCol + gHeight/(gcLength+1) + "px";
+    else
+        g.style.height = gHeight/mGridCol + gHeight/(gcLength+1) + "px";
+}
+
+["resize", "load"].forEach(function(event) {
+    window.addEventListener(event, function () {
+        imagesLoaded(document.querySelector('.masonry'), function () {
+            // A maonsry grid with 8px gutter, with 3 columns on desktop, 2 on tablet, and 1 column on mobile devices.
+            masonry(".masonry", ".masonry-brick", 8, 3, 2, 1);
+        });
     });
 });
